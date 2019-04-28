d228e6f63fbda1194051c8f9918849e96d26ed21
walterlee <walter.lee9@gmail.com>
2019-04-28 15:59:58 -0500

    Updates

---
+++ precache-manifest.e8cee1d18393af24707b3564dedebd06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797eefe64b2c8ece1ced7633e2c28825",
+    "url": "/react-us-state-capital-quiz/index.html"
+  },
+  {
+    "revision": "ba730b5200d375fec63c",
+    "url": "/react-us-state-capital-quiz/static/css/main.9c5b91f3.chunk.css"
+  },
+  {
+    "revision": "7fd541373f8f019a9af7",
+    "url": "/react-us-state-capital-quiz/static/js/2.b2b13d96.chunk.js"
+  },
+  {
+    "revision": "ba730b5200d375fec63c",
+    "url": "/react-us-state-capital-quiz/static/js/main.55496020.chunk.js"
+  },
+  {
+    "revision": "caa36a7908222ad6687b",
+    "url": "/react-us-state-capital-quiz/static/js/runtime~main.def74511.js"
+  }
+]);
